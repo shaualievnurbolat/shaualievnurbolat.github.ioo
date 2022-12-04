0a6e205071c01ee675978bb9e21c3ad7b0d16480
Nurbolat <shaualiev.ns@gmail.com>
2022-12-04 12:54:19 +0600

    adaptive

--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 import './scss/style.scss'
 import images from './img/monument.png'
-// import "../node_modules/animate.css";
 
 console.log('hello world')
 
 const ABOUTMEWRAPPER = document.querySelector('.aboutme__wrapper')
 
-ABOUTMEWRAPPER.insertAdjacentHTML('beforeend', `<img class="aboutme__wrapper-bg animate__animated animate__fadeInRight" src=${images}>`)
+ABOUTMEWRAPPER.insertAdjacentHTML('beforeend', `<img class="aboutme__wrapper-bg animate__animated animate__fadeInRightBig" src=${images}>`)
 
-window.addEventListener('scroll', function() {
-    document.getElementById('showScroll').innerHTML = '100vh';
-  });
+// window.addEventListener('scroll', function() {
+//     window.scrollBy(0, window.innerHeight)
+// });
